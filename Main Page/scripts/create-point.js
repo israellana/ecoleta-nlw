07e96733c7dbbfb9d5b4f478c6040967dcf162c4
Israel Lana <israellana_@hotmail.com>
2020-06-02 16:43:49 -0300

    day 2

---
+++ Main Page/scripts/create-point.js
@@ -0,0 +1,40 @@
+
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/distritos")
+    .then(res => res.json())
+    .then(states => {
+
+        for(state of states) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+    })
+}
+
+populateUFs()
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateSelect = document.querySelector("input[name=state]")
+    const ufValue = event.target.value
+
+    indexOfSelectedState = event.target.selectedIndex
+    stateSelect.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+    .then(res => res.json())
+    .then(cities => {
+
+        for(ciry of cities) {
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    })
+}
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
